3922c57cb9c44bed9c35d254985af0a03a34fcf4
David Reinberger <davidreinberger@gmail.com>
2018-08-27 17:37:02 +0200

    add logs

--- bin/vue-cli-service.js
+++ bin/vue-cli-service.js
@@ -4,6 +4,8 @@ const semver = require('semver')
 const { error } = require('@vue/cli-shared-utils')
 const requiredVersion = require('../package.json').engines.node
 
+console.log('1')
+
 if (!semver.satisfies(process.version, requiredVersion)) {
   error(
     `You are using Node ${process.version}, but vue-cli-service ` +
@@ -12,8 +14,15 @@ if (!semver.satisfies(process.version, requiredVersion)) {
   process.exit(1)
 }
 
+console.log('2')
+
 const Service = require('../lib/Service')
+
+console.log('3')
+
 const service = new Service(process.env.VUE_CLI_CONTEXT || process.cwd())
+
+console.log('4')
 
 const rawArgv = process.argv.slice(2)
 const args = require('minimist')(rawArgv, {
@@ -32,6 +41,8 @@ const args = require('minimist')(rawArgv, {
   ]
 })
 const command = args._[0]
+
+console.log('5')
 
 service.run(command, args, rawArgv).catch(err => {
   error(err)
